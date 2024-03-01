073414f6b07de12ad0ce98a77e52e897c95c154b
TheLukCraft <luk1997r@o2.pl>
2024-03-01 18:18:52 +0100

    added more texts

---
+++ ads/script.js
@@ -0,0 +1,18 @@
+let texts = [
+    "1000x = 5 punktów",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f"
+];
+let currentIndex = 0;
+
+function changeText() {
+    currentIndex = (currentIndex - 1) % texts.length;
+    document.getElementById("text").innerText = texts[currentIndex];
+}
+
+//set intervals 5000 = 5s
+setInterval(changeText, 5000);
