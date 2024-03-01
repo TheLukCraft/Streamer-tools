3e2b012f23e6819909cc3f16f14a4f36520bdb86
TheLukCraft <luk1997r@o2.pl>
2024-03-01 21:57:57 +0100

    added more texts and icons

--- ads/script.js
+++ ads/script.js
@@ -1,10 +1,15 @@
 let texts = [
     " 1000x = 5 punktów",
+    " Streamy codziennie 20:00",
+    " Donate w BIO 1zł to 25 pkt",
     " !help - komendy",
     " !sounds - nowe dźwięki",
     " !top - sprawdź topkę",
     " !shop - sklep za punkty",
-    " Streamy codziennie 20:00"
+    " !daily - 10 pkt za darmo",
+    " !spin - kręć kołem fortuny",
+    " !awards - nagrody dla top 3",
+    
 ];
 let currentIndex = 0;
 
@@ -14,19 +19,31 @@ function changeText() {
             newText = '<i class="fas fa-heart"></i>' + texts[currentIndex];
             break;
         case 1:
-            newText = '<i class="fas fa-comments"></i>' + texts[currentIndex];
+            newText = '<i class="fas fa-calendar-alt"></i>' + texts[currentIndex];
             break;
         case 2:
-            newText = '<i class="fas fa-music"></i>' + texts[currentIndex];
+            newText = '<i class="fa-solid fa-circle-dollar-to-slot"></i>' + texts[currentIndex];
             break;
         case 3:
-            newText = '<i class="fas fa-star"></i>' + texts[currentIndex];
+            newText = '<i class="fas fa-comments"></i>' + texts[currentIndex];
             break;
         case 4:
-            newText = '<i class="fas fa-shopping-cart"></i>' + texts[currentIndex];
+            newText = '<i class="fas fa-music"></i>' + texts[currentIndex];
             break;
         case 5:
-            newText = '<i class="fas fa-calendar-alt"></i>' + texts[currentIndex];
+            newText = '<i class="fas fa-star"></i>' + texts[currentIndex];
+            break;
+        case 6:
+            newText = '<i class="fas fa-shopping-cart"></i>' + texts[currentIndex];
+            break;
+        case 7:
+            newText = '<i class="fa-solid fa-gift"></i>' + texts[currentIndex];
+            break;
+        case 8:
+            newText = '<i class="fa-solid fa-dice"></i>' + texts[currentIndex];
+            break;
+        case 9:
+            newText = '<i class="fa-solid fa-award"></i>' + texts[currentIndex];
             break;
         default:
             newText = texts[currentIndex];
