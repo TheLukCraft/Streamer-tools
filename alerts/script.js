a523bd0ba29f1011dcd5d8ffc3529b2d9a75b90f
TheLukCraft <luk1997r@o2.pl>
2024-03-05 05:09:04 +0100

    update

--- alerts/script.js
+++ alerts/script.js
@@ -1,10 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     let commands = [
-        { text: "Progi gift: 1|10 | 50 | 100 | 500| 1k", icon: "fas fa-gift" },
-        { text: "5k | 10k | 25k | 40k monet" },
+        { text: "Progi gift: 1 | 10 | 50 | 100 | 500", icon: "fas fa-gift" },
+        { text: "| 1k | 5k | 10k | 25k | 40k monet" },
         { text: "monety = ilość punktów (suby 2x)", icon: "fas fa-exchange-alt" },
         { text: "Progi donate: 1zł | 5 zł | 10zł | 20zł", icon: "fas fa-donate" },
-        { text: "21,37zł | 50zł | 100zł | 200zł | 1000zł"},
+        { text: "| 21,37zł | 50zł | 100zł | 200zł | 1000zł"},
         { text: "1zł = 25 punktów (suby 2x)", icon: "fas fa-calculator" }
     ];
     const container = document.querySelector('.commands-container');
