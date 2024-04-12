2822b521df8daa839a44736fcb62be1136c18ea3
TheLukCraft <luk1997r@o2.pl>
2024-04-12 20:15:53 +0200

    update texts

--- shop/script.js
+++ shop/script.js
@@ -1,16 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
     let commands = [
-        { text: "!turtle żółwik dla widza (100pkt)", icon: "fas fa-hand-paper" },
-        { text: "!leaf dostajesz z liścia (200pkt)", icon: "fas fa-leaf" },
-        { text: "!coaching ocenka konta na op.gg (300pkt)", icon: "fas fa-chart-line" },
-        { text: "!autograph podpis Steam (400pkt)", icon: "fas fa-signature" },
-        { text: "!photo fotka z Tobą na DC (500pkt)", icon: "fas fa-camera" },
+        { text: "!turtle żółwik dla widza (10pkt)", icon: "fas fa-hand-paper" },
+        { text: "!leaf dostajesz z liścia (50pkt)", icon: "fas fa-leaf" },
+        { text: "!rateme ocenka konta na op.gg (30pkt)", icon: "fas fa-chart-line" },
+        { text: "!autograph podpis Steam (100pkt)", icon: "fas fa-signature" },
+        { text: "!photo fotka z Tobą na DC (250pkt)", icon: "fas fa-camera" },
         { text: "!duo gra nierankingową w lolu (700pkt)", icon: "fas fa-gamepad" },
         { text: "!getgame dostajesz grę !games (1k pkt)", icon: "fas fa-gift" },
         { text: "!spinpoints rozlosuj swoje (1k pkt)", icon: "fas fa-random" },
         { text: "!greetings pozdrownia wideo (2k pkt)", icon: "fas fa-video" },
         { text: "!recordvideo tiktoka o Twoim champ (3k)", icon: "fas fa-mobile-alt" },
-        { text: "!rateme analiza demka + porady (5k pkt)", icon: "fas fa-search" },
+        { text: "!coaching analiza demka + porady (5k pkt)", icon: "fas fa-search" },
         { text: "!meetingkrk wspólny obiad w Krk (30k pkt)", icon: "fas fa-utensils" },
         { text: "!meeting przyjadę do Ciebie + ^ (80k pkt)", icon: "fas fa-truck" }
     ];
